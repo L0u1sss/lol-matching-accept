7a69b1d318812c7d4db8302de7ceaf33e98b0e3f
L0u1sss <113091598+L0u1sss@users.noreply.github.com>
2024-04-07 02:09:11 +0700

    Add files via upload

--- lol.py
+++ lol.py
@@ -1,9 +1,10 @@
 import pyautogui
+
 def lol_button():
     print('หาห้องอยู่')
     while True:
         try:
-            location = pyautogui.locateOnScreen('accept.png',confidence=0.6)
+            location = pyautogui.locateOnScreen('lol/accept.png')
             print('เจอห้องแล้ว')
             pyautogui.moveTo(location)
             pyautogui.click(location)
